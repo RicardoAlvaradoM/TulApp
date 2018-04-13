cfa9125b05076252a6b0db9bd1f3313d9fcba0c3
Ricardo jhovani Alvarado Miranda <ricardo.miranda3496@gmail.com>
2018-04-12 21:21:37 -0500

    Cambios de auto-redimension en algunas paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,7 +45,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      timer(3000).subscribe(() => this.showSplash = false) ;
+      timer(3000).subscribe(() => this.showSplash = false)
     });
   }
 
