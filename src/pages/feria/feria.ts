d6461773dd8c66a37faf7c8e557ee0670cfb8c6a
Ricardo jhovani Alvarado Miranda <ricardo.miranda3496@gmail.com>
2018-04-17 13:42:01 -0500

    cambios s

--- src/pages/feria/feria.ts
+++ src/pages/feria/feria.ts
@@ -1,12 +1,7 @@
 import { Component,ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
 import { DomSanitizer } from '@angular/platform-browser';
-/**
- * Generated class for the FeriaPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
